f951177dadc717c41f2ec1cfb2d8d7293ec56f0d
Helin Okcu <helinnokcu@gmail.com>
2024-02-16 19:33:57 +0300

    firebase added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDG6h0mPjLtyfXh8E3G45jctU6_F1Wu1tY',
+  authDomain: 'blog-project-8481d.firebaseapp.com',
+  projectId: 'blog-project-8481d',
+  storageBucket: 'blog-project-8481d.appspot.com',
+  messagingSenderId: '15026441755',
+  appId: '1:15026441755:web:39dcacdbca697c503a32be',
+  measurementId: 'G-HFVRZC462H',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
